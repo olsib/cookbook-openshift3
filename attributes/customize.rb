347fedf3fa729ea21ab1acab186b0e9008c5c356
Olsi Birbo <olsi.birbo@mdevlab.com>
2016-09-05 13:59:00 +0000

    changed some attributes

--- attributes/customize.rb
+++ attributes/customize.rb
@@ -1,4 +1,12 @@
-default['cookbook-openshift3']['master_servers'] = [{"fqdn" => node["opsworks"]["instance"]["hostname"], "ipaddress" => node["opsworks"]["instance"]["ip"]}]
-default['cookbook-openshift3']['node_servers'] = [{"fqdn" => node["opsworks"]["instance"]["hostname"], "ipaddress" => node["opsworks"]["instance"]["ip"]}]
+require 'chef/log'
+Chef::Log.level = :debug
+
+#Chef::Log.info(node["opsworks"]["instance"]["hostname"])
+
+default['cookbook-openshift3']['master_servers'] = [{"fqdn" => node["opsworks"]["instance"]["hostname"]+".localdomain", "ipaddress" => node["opsworks"]["instance"]["private_ip"]}]
+default['cookbook-openshift3']['node_servers'] = [{"fqdn" => node["opsworks"]["instance"]["hostname"]+".localdomain", "ipaddress" => node["opsworks"]["instance"]["private_ip"]}]
+
 default['cookbook-openshift3']['oauth_Identity'] = 'LDAPPasswordIdentityProvider'
-default['cookbook-openshift3']['openshift_master_identity_provider']['LDAPPasswordIdentityProvider'] = [{"name" => "ldap_identity","login" => true,"challenge" => true, "kind" => "LDAPPasswordIdentityProvider", "ldap_server" => "directory.mdevlab.com", "ldap_bind_dn" => "CN=openshift,OU=Service Accounts,OU=Markit,DC=directory,DC=mdevlab,DC=com", "ldap_bind_password" => "Welcom1", "ldap_insecure" => true,"ldap_base_ou" => "ou=Standard Users,ou=Markit,dc=directory,dc=mdevlab,dc=com?sAMAccountName", "ldap_preferred_username" => "sAMAccountName"}]
+#default['cookbook-openshift3']['openshift_master_identity_provider']['LDAPPasswordIdentityProvider'] = [{"name" => "ldap_identity","login" => true,"challenge" => true, "kind" => "LDAPPasswordIdentityProvider","ldap_server" => "directory.mdevlab.com","ldap_bind_dn" => "CN=openshift,OU=Service Accounts,OU=Markit,DC=directory,DC=mdevlab,DC=com", "ldap_bind_password" => "Welcome1", "ldap_insecure" => true,"ldap_base_ou" => "ou=Standard Users,ou=Markit,dc=directory,dc=mdevlab,dc=com?sAMAccountName", "ldap_preferred_username" => "sAMAccountName"}]
+
+default['cookbook-openshift3']['openshift_master_identity_provider']['LDAPPasswordIdentityProvider'] = { 'name' => 'ldap_identity', 'login' => true, 'challenge' => true, 'kind' => 'LDAPPasswordIdentityProvider', 'ldap_server' => 'directory.mdevlab.com', 'ldap_bind_dn' => 'CN=openshift,OU=Service Accounts,OU=Markit,DC=directory,DC=mdevlab,DC=com', 'ldap_bind_password' => 'Welcome1', 'ldap_insecure' => true, 'ldap_base_ou' => 'ou=Standard Users,ou=Markit,dc=directory,dc=mdevlab,dc=com', 'ldap_preferred_username' => 'sAMAccountName' }
--- attributes/identity_provider.rb
+++ attributes/identity_provider.rb
@@ -1,8 +1,11 @@
-default['cookbook-openshift3']['oauth_Identity'] = 'HTPasswdPasswordIdentityProvider'
+default['cookbook-openshift3']['oauth_Identity'] = 'LDAPPasswordIdentityProvider'
 
 default['cookbook-openshift3']['openshift_master_identity_provider']['HTPasswdPasswordIdentityProvider'] = { 'name' => 'htpasswd_auth', 'login' => true, 'challenge' => true, 'kind' => 'HTPasswdPasswordIdentityProvider', 'filename' => "#{default['cookbook-openshift3']['openshift_common_master_dir']}/openshift-passwd" }
 
-default['cookbook-openshift3']['openshift_master_identity_provider']['LDAPPasswordIdentityProvider'] = { 'name' => 'ldap_identity1', 'login' => true, 'challenge' => true, 'kind' => 'LDAPPasswordIdentityProvider', 'ldap_server' => 'ldap1.domain.local', 'ldap_bind_dn' => '', 'ldap_bind_password' => '', 'ldap_insecure' => true, 'ldap_base_ou' => 'OU=people,DC=domain,DC=local', 'ldap_preferred_username' => 'uid' }
+#default['cookbook-openshift3']['openshift_master_identity_provider']['LDAPPasswordIdentityProvider'] = { 'name' => 'ldap_identity1', 'login' => true, 'challenge' => true, 'kind' => 'LDAPPasswordIdentityProvider', 'ldap_server' => 'ldap1.domain.local', 'ldap_bind_dn' => '', 'ldap_bind_password' => '', 'ldap_insecure' => true, 'ldap_base_ou' => 'OU=people,DC=domain,DC=local', 'ldap_preferred_username' => 'uid' }
+
+default['cookbook-openshift3']['openshift_master_identity_provider']['LDAPPasswordIdentityProvider'] = { 'name' => 'ldap_identity', 'login' => true, 'challenge' => true, 'kind' => 'LDAPPasswordIdentityProvider', 'ldap_server' => 'directory.mdevlab.com', 'ldap_bind_dn' => 'CN=openshift,OU=Service Accounts,OU=Markit,DC=directory,DC=mdevlab,DC=com', 'ldap_bind_password' => 'Welcome1', 'ldap_insecure' => true, 'ldap_base_ou' => 'ou=Standard Users,ou=Markit,dc=directory,dc=mdevlab,dc=com', 'ldap_preferred_username' => 'sAMAccountName' }
+
 
 default['cookbook-openshift3']['openshift_master_identity_provider']['RequestHeaderIdentityProvider'] = { 'name' => 'header_provider_identify', 'login' => false, 'challenge' => false, 'kind' => 'RequestHeaderIdentityProvider', 'headers' => %w(X-Remote-User SSO-User), 'clientCA' => "#{default['cookbook-openshift3']['openshift_common_master_dir']}/master/ca.crt" }
 
